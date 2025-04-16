b50a58028dae5f172d5b5406684cc6e484463d1f
Subha Mandal <168220649+SubhaMandal@users.noreply.github.com>
2025-04-16 23:21:00 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+boy = 22
+if(boy==22):
+    print("the boy is a audlt")
+else:
+    print("the boy is not an adult")
+    
+    
+girl =23
+if (girl == 17):
+    print("the girl is a kid")
+elif (girl > 17 & girl <20):
+    print("the girl is stil young")
+else:
+    print("the gilr is an adult")
