d7f707311b6ccb287824857d933e90c4523be735
Subha Mandal <168220649+SubhaMandal@users.noreply.github.com>
2025-04-16 23:18:00 +0530

    Add files via upload

---
+++ functionReturn.py
@@ -0,0 +1,12 @@
+def newFunction(a,b):
+    return(a+b)
+a = newFunction(2,3)
+print(a)
+
+# a new example using input
+
+def newFunction2(c,d):
+    print(c+d)
+x= input("enter the 1st number ")
+Y= input("enter the 2nd number ")
+newFunction2(x,Y)
