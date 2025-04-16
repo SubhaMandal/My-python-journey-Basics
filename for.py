d7754d18a55831a2a376d5bf689c3458ee78384b
Subha Mandal <168220649+SubhaMandal@users.noreply.github.com>
2025-04-16 23:20:21 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    print(i)
+    
+for i in "Astha":
+    print(i)
+    
+'''
+
+✔ Start = 10 (first number)
+✔ Stop = -1 (stops before reaching -1)
+✔ Step = -1 (decreases by 1 each time)
+
+
+'''
+for i in range(10, -1, -1):
+    print(i)
