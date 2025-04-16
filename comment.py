9764cabcb76357a06925369b1cee755752b342d7
Subha Mandal <168220649+SubhaMandal@users.noreply.github.com>
2025-04-16 23:20:00 +0530

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,10 @@
+# this is a single line comment which will not print 
+'''
+a = 7
+b = 8
+print (a+b)
+
+this is a multi line comment which we use when we need multiple lines of 
+code need to be ignor/ not executed 
+
+'''
